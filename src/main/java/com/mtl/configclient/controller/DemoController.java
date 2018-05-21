4a1c1ddb8fb675035a893835a3b7ec6d5248c3c1
mutongliwansui <1218250915@qq.com>
2018-05-21 16:36:47 +0800

    Initial commit

---
+++ src/main/java/com/mtl/configclient/controller/DemoController.java
@@ -0,0 +1,28 @@
+package com.mtl.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class DemoController {
+    @Value("${from}")
+    private String from;
+
+    @Autowired
+    private Environment environment;
+
+    @RequestMapping("/from")
+    public String from(){
+        return from;
+    }
+
+    @RequestMapping("/fromenv")
+    public String fromenv(){
+        return environment.getProperty("from","undefined");
+    }
+}
